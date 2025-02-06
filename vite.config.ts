3094fad3e5243b30ec408559eff336699a23bb94
Pål Syvertsen Stakvik <paalss94@gmail.com>
2025-02-06 08:12:49 +0000

    tasks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["8ypcmr-5173.csb.app"],
+  },
+});
